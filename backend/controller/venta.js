8db9429502f74d1976e98bf51de88a88085224e7
Cesar Roman Reyes <src_graff@hotmail.com>
2019-07-27 09:21:00 -0500

    metodo guardar en el controller

--- backend/controller/venta.js
+++ backend/controller/venta.js
@@ -1,4 +1,5 @@
 var Venta=require('../models/venta');
+var Producto=require('../models/producto');
 
 var controller={
     saveVenta:(req,res)=>{
@@ -7,7 +8,27 @@ var controller={
 
         venta.fecha=params.fecha;
         venta.cliente=params.cliente;
-        venta.carrito=params.carrito;
+        venta.productos=params.productos;
+        
+        // params.productos.forEach(element => {
+        //     if(validarExistencia(element.producto.id,element.cantidad)){
+        //         venta.productos.append({
+        //             producto:element.producto.id,
+        //             cantidad:element.cantidad
+        //         });
+        //     }
+        // });
+
+        // let validarExistencia=(id_producto,cantidad)=>{
+        //     Producto.findById(id_producto).exec((err,data)=>{
+        //         if(data.no_existencias>=cantidad){
+        //             return true;
+        //         }else{
+        //             return false;
+        //         }
+        //     });
+        // }
+
 
         venta.save((err,venta)=>{
             if(err) res.status(500).send({mensaje:'error en el servidor'});
